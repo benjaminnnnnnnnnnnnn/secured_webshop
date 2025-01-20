7dfbd8ad3f393ac27c65c30bfda1765ee93802c6
benjaminnnnnnnnnnnnn <px25twk@eduvaud.ch>
2025-01-20 15:03:33 +0100

    update

--- app/controllers/UserController.js
+++ app/controllers/UserController.js
@@ -1,5 +1,17 @@
 module.exports = {
     get: (req, res) => {
-        res.send("User: Sarah Test");
+        res.send(`
+
+            <div style="display: flex; justify-content: center; align-items: center; height: 50vh;">
+                <form action="/User" method="post" style="text-align: center;">
+                    <h1 style="text-align: center;">Login</h1>
+                    <label for="username">Username:</label>
+                    <input type="text" id="username" name="username"><br><br>
+                    <label for="password">Password:</label>
+                    <input type="password" id="password" name="password"><br><br>
+                    <button type="connect">Connect</button>
+                </form>
+            </div>
+        `);
     }
 };
--- app/server.js
+++ app/server.js
@@ -1,4 +1,11 @@
 const express = require("express");
+const https = require('https');
+const fs = require('fs');
+
+const options = {
+  key: fs.readFileSync('selfsigned.key'),
+  cert: fs.readFileSync('selfsigned.crt')};
+
 
 
 const app = express();
@@ -7,7 +14,11 @@ app.use('/user', userRoute);
 
 
 
+
+
 // Démarrage du serveur
-app.listen(8080, () => {
+app.listen(8085, () => {
     console.log('Server running on port 8080');
 });
+
+    https.createServer(options, app).listen(8443);
