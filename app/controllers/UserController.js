73db2a5b42c27843a3579cda6e83b8312cafdd2e
benjaminnnnnnnnnnnnn <px25twk@eduvaud.ch>
2025-02-24 14:27:33 +0100

    HTTPS/user page update/admin page update

--- app/controllers/UserController.js
+++ app/controllers/UserController.js
@@ -10,7 +10,7 @@ module.exports = {
     },
 
     post: (req, res) => {
-        connection.query(`SELECT password, isAdmin FROM users WHERE username = ?`, [req.body.username], (err, results) => {
+        connection.query(`SELECT username, password, isAdmin FROM Users WHERE username = ?`, [req.body.username], (err, results) => {
             if (err) throw err;
 
             if (results.length > 0) {
@@ -18,11 +18,7 @@ module.exports = {
                     if (err) throw err;
 
                     if (result) {
-                        if (results[0].isAdmin === 1) {
-                            res.sendFile(path.join(__dirname, '../pages/admin.html'));
-                        } else {
-                            res.sendFile(path.join(__dirname, '../pages/user.html'));
-                        }
+                        res.json({ isAdmin: results[0].isAdmin === 1, username: results[0].username });
                     } else {
                         res.status(401).json({ error: 'Invalid username or password' });
                     }
@@ -34,27 +30,35 @@ module.exports = {
     },
 
     register: (req, res) => {
-        connection.query(`SELECT COUNT(*) FROM users WHERE username = ?`, [req.body.username], (err, results) => {
+        connection.query(`SELECT COUNT(*) FROM Users WHERE username = ?`, [req.body.username], (err, results) => {
             if (err) throw err;
             if (results[0]['COUNT(*)'] > 0) {
                 res.status(401).json({ error: 'Username already exists' });
                 return;
             }
 
-            connection.query(`SELECT MAX(id) FROM users`, (err, results) => {
+            connection.query(`SELECT MAX(id) FROM Users`, (err, results) => {
                 if (err) throw err;
                 const newid = (results[0]['MAX(id)'] + 1);
 
-                const { username, password } = req.body;
+                const { username, password } = req.body; 
                 bcrypt.hash(password, saltRounds, (err, hash) => {
                     if (err) throw err;
 
-                    connection.query(`INSERT INTO users (id, username, password, isAdmin) VALUES (?, ?, ?, 0)`, [newid, username, hash], (err) => {
+                    connection.query(`INSERT INTO Users (id, username, password, isAdmin) VALUES (?, ?, ?, 0)`, [newid, username, hash], (err) => {
                         if (err) throw err;
                         res.sendFile(path.join(__dirname, '../pages/user.html'));
                     });
                 });
             });
+        });
+    },
+
+    getUsers: (req, res) => {
+        const searchQuery = req.query.search ? `%${req.query.search}%` : '%';
+        connection.query(`SELECT id,username,password,isAdmin FROM Users WHERE username LIKE ?`, [searchQuery], (err, results) => {
+            if (err) throw err;
+            res.json(results);
         });
     }
 };
--- app/server.js
+++ app/server.js
@@ -2,38 +2,20 @@ const express = require("express");
 const path = require("path");
 const app = express();
 
+const fs = require("fs");
+const https = require("https");
+
+const sslOptions = {
+  key: fs.readFileSync("./privkey.key"),
+  cert: fs.readFileSync("./certificate.crt"),
+};
+
 // Add this line to parse JSON bodies
 app.use(express.json());
 
 // Add this line to parse URL-encoded bodies
 app.use(express.urlencoded({ extended: true }));
-/*
-const mysql = require("mysql2");
 
-// Create a connection to the database
-const connection = mysql.createConnection({
-	host: "localhost",
-	user: "root",
-	password: "root",
-	database: "db",
-	port: 6033,
-});
-
-// Connect to the database
-connection.connect((err) => {
-	if (err) throw err;
-	console.log("Connected to MySQL Database!");
-
-	// Example query
-	connection.query("SELECT * FROM users", (err, results, fields) => {
-		if (err) throw err;
-		console.log(results);
-	});
-
-	// connection.release();
-});
-
-*/
 const userRoute = require("./routes/User");
 
 app.use("/user", userRoute);
@@ -41,16 +23,17 @@ app.use("/user", userRoute);
 app.use(express.static(path.join(__dirname, "pages")));
 
 app.get("/login", (req, res) => {
-	res.sendFile(path.join(__dirname, "pages", "login.html"));
+  res.sendFile(path.join(__dirname, "pages", "login.html"));
 });
 
 app.get("/Sign-up", (req, res) => {
-	res.sendFile(path.join(__dirname, "pages", "signup.html"));
+  res.sendFile(path.join(__dirname, "pages", "signup.html"));
 });
 
+const userController = require('./controllers/UserController');
 
+app.get('/admin/users', userController.getUsers);
 
-// Start the server
-app.listen(8085, () => {
-	console.log("Server running on port 8085");
+https.createServer(sslOptions, app).listen(443, () => {
+  console.log("Server running on port 443");
 });
